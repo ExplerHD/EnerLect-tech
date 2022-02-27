121d76241031ef71e151dc8c5b3e3f386b154a1d
ExplerHD <73381264+ExplerHD@users.noreply.github.com>
2022-02-27 21:34:21 +0700

    Makes block.

---
+++ src/enerlect/content/EnerLectBlocks.java
@@ -0,0 +1,51 @@
+package enerlect.content;
+
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.ctype.*;
+import mindustry.game.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.entities.bullet.*;
+import mindustry.world.*;
+import mindustry.world.blocks.*;
+import mindustry.world.blocks.power.*;
+import mindustry.world.blocks.units.*;
+import mindustry.world.blocks.defense.*;
+import mindustry.world.blocks.defense.turrets.*;
+import mindustry.world.blocks.production.*;
+import mindustry.world.blocks.distribution.*;
+import mindustry.world.meta.*;
+import mindustry.world.draw.*;
+
+import enerlect.content.*;
+
+import static mindustry.type.ItemStack.with;
+
+public class EnerLectBlocks implements ContentList {
+  public static Block
+    circleator;
+  
+  @Override
+  public void load() {
+    implosion = new PowerTurret("circleator"){{
+      requirements(Category.turret, with(
+        Items.copper, 50,
+        Items.lead, 50,
+        Items.plastanium, 60
+      ));
+      size = 2;
+      health = 50 * size * size;
+      reloadTime = 90f;
+      range = 135f;
+      powerUse = 2.7f;
+      restitution = 0.05f;
+      shootType = EnerLectBullets.implosion;
+    }};
+  }
+}
