0617d6f069de3b9ea1dd31f84fde628e29a26009
ExplerHD <73381264+ExplerHD@users.noreply.github.com>
2022-02-28 19:00:21 +0700

    Create EnerLectFX.java

---
+++ src/enerlect/content/EnerLectFX.java
@@ -0,0 +1,33 @@
+package enerlect.content;
+
+import arc.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.math.geom.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.entities.*;
+import mindustry.game.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.world.Block;
+
+import static arc.graphics.g2d.Draw.rect;
+import static arc.graphics.g2d.Draw.*;
+import static arc.graphics.g2d.Lines.*;
+import static arc.math.Angles.*;
+import static mindustry.Vars.*;
+public class Fx{
+  private static final Rand rand = new Rand();
+  private static final Vec2 v = new Vec2();
+  
+  public static final Effect
+    
+  implosion = new Effect(60f, e -> {
+    color(Color.blue);
+    stroke(3f * e.fout());
+    Lines.poly(e.x, e.y, 6, e.rotation + e.fin);
+  })followParent(true);
+}
