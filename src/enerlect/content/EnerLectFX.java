616e5719e7bca8473d73678f6a6f45ace02d3cc5
ExplerHD <73381264+ExplerHD@users.noreply.github.com>
2022-02-28 19:09:20 +0700

    Update EnerLectFX.java

--- src/enerlect/content/EnerLectFX.java
+++ src/enerlect/content/EnerLectFX.java
@@ -19,7 +19,7 @@ import static arc.graphics.g2d.Draw.*;
 import static arc.graphics.g2d.Lines.*;
 import static arc.math.Angles.*;
 import static mindustry.Vars.*;
-public class Fx{
+public class EnerLectFX{
   private static final Rand rand = new Rand();
   private static final Vec2 v = new Vec2();
   
@@ -28,6 +28,6 @@ public class Fx{
   implosion = new Effect(60f, e -> {
     color(Color.blue);
     stroke(3f * e.fout());
-    Lines.poly(e.x, e.y, 6, e.rotation + e.fin);
+    Lines.poly(e.x, e.y, 6, e.rotation + e.fin());
   }).followParent(true);
 }
