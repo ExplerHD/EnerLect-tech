99349fd90a6119537e5f4461d73851c5c838baec
1237 <92003875+12three7@users.noreply.github.com>
2022-02-28 09:33:39 +0800

    Update EnerLectBlocks.java

--- src/enerlect/content/EnerLectBlocks.java
+++ src/enerlect/content/EnerLectBlocks.java
@@ -30,7 +30,7 @@ import enerlect.content.*;
 
 import static mindustry.type.ItemStack.with;
 
-public class EnerLectBlocks implements ContentList {
+public class EnerLectBlocks implements ContentList{
   public static Block
     implosion;
   
@@ -50,7 +50,6 @@ public class EnerLectBlocks implements ContentList {
       restitution = 0.05f;
       shootType = new LaserBulletType(145){{ /* contoh code laser bullet */
         colors = new Color[]{Pal.darkishGray.a(1f)};
-        hitEffect = Fx.hitlancer;
         hitSize = 6;
         lifetime = 36f;
         drawSize = 200f;
