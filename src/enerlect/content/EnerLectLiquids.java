b8a8ce1a5f8be9c71228af38f325c76f24312c10
ExplerHD <73381264+ExplerHD@users.noreply.github.com>
2022-03-03 08:01:08 +0700

    Update EnerLectLiquids.java

--- src/enerlect/content/EnerLectLiquids.java
+++ src/enerlect/content/EnerLectLiquids.java
@@ -1,7 +1,7 @@
 package enerlect.content;
 
-import arc.graphics.*;
-import mindustry.ctype.*;
+import arc.graphics.Color;
+import mindustry.ctype.ContentList;
 import mindustry.type.*;
 
 public class EnerLectLiquids implements ContentList{
@@ -10,8 +10,7 @@ public class EnerLectLiquids implements ContentList{
   @Override
   public void load(){
     electricLiquid = new Liquid("electric-liquid", Color.valueOf("f5372a")){{
-      heatCapacity = 0.4f;
-      effect = StatusEffects.shocked;
+      viscosity = 1.2f;
     }};
   }
 }
