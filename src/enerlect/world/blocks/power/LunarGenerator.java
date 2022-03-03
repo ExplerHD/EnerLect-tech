8c54c4c87091cfd7e24f67dae12cf8fdc6772871
ExplerHD <73381264+ExplerHD@users.noreply.github.com>
2022-03-03 17:27:54 +0700

    Tries Custom Type

---
+++ src/enerlect/world/blocks/power/LunarGenerator.java
@@ -0,0 +1,33 @@
+package enerlect.world.blocks.power;
+
+import arc.math.*;
+import arc.struct.*;
+import mindustry.world.meta.*;
+
+import static mindustry.Vars.*;
+public class LunarGenerator extends PowerGenerator{
+  
+  public LunarGenerator(string name){
+    super(name);
+    flags = EnumSet.of();
+    envEnabled = Env.any;
+  }
+  
+  public void setStats(){
+    super.(setStats);
+    stats.remove(generationType);
+    stats.add(generationType, powerProduction * 60.0f, StatUnit.powerSecond);
+  }
+  
+  public class LunarGeneratorBuild extends GeneratorBuild{
+    @Override
+    public void updateTile{
+      productionEfficiency = enabled ?
+        Mathf.maxZero(Attribute.light.env() + 
+                      (state.rules.lighting ?
+                       0.1f + state.rules.ambientLight.a :
+                       1f
+        )) : 0f;
+    }
+  }
+}
