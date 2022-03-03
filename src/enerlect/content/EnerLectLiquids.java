2f2eeaaab240e21e9219c0c1dd4e87e5446673b0
ExplerHD <73381264+ExplerHD@users.noreply.github.com>
2022-03-03 21:57:59 +0700

    One branches with items

--- src/enerlect/content/EnerLectLiquids.java
+++ src/enerlect/content/EnerLectLiquids.java
@@ -4,11 +4,16 @@ import arc.graphics.Color;
 import mindustry.ctype.ContentList;
 import mindustry.type.*;
 
-public class EnerLectLiquids implements ContentList{
+public class EnerLectItems implements ContentList{
+  public static Items darkenedMetal;
   public static Liquid electricLiquid;
   
   @Override
   public void load(){
+    darkenedMetal = new Item("darkened-metal", Color.valueOf("6b6464")){{
+      cost = 4;
+    }};
+    
     electricLiquid = new Liquid("electric-liquid", Color.valueOf("f5372a")){{
       viscosity = 1.2f;
     }};
