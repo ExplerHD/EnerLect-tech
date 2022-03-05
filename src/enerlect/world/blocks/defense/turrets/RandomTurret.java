645f8b407aa792a2bbd9aaf154df44e9a465129a
1237 <92003875+12three7@users.noreply.github.com>
2022-03-05 12:39:43 +0800

    Update RandomTurret.java

--- src/enerlect/world/blocks/defense/turrets/RandomTurret.java
+++ src/enerlect/world/blocks/defense/turrets/RandomTurret.java
@@ -14,12 +14,16 @@ import mindustry.type.*;
 import mindustry.ui.*;
 import mindustry.world.consumers.*;
 import mindustry.world.meta.*;
-import mindustry.world.blocks.defense.turrets;
 import mindustry.world.blocks.defense.turrets.*;
 import mindustry.world.blocks.defense.*;
 import enerlect.content.*;
 import enerlect.world.blocks.*;
-
+import mindustry.Vars.*;
+import mindustry.ctype.*;
+import mindustry.ctype.Content;
+import mindustry.ctype.ContentList;
+import mindustry.ctype.ContentType;
+import mindustry.core.*;
 import static mindustry.Vars.*;
 
 public class RandomTurret extends ItemTurret{ // kool random turret code
