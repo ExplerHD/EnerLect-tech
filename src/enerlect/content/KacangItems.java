e91c66f462d0230420604b4104848fbdec1bac8b
1237 <92003875+12three7@users.noreply.github.com>
2022-03-05 18:32:33 +0800

    Create KacangItems.java

---
+++ src/enerlect/content/KacangItems.java
@@ -0,0 +1,22 @@
+import arc.graphics.Color;
+import mindustry.ctype.ContentList;
+import mindustry.type.*;
+import enerlect.content.*;
+
+public class KacangItems implements ContentList{
+	public 
+  public static Item kacang, kacangHijau;
+	@Override
+  	public void load(){
+		// items
+		kacang = new Item("kacang", Color.valueOf("edcc93")){{
+			cost = 0;
+			hardness  = 1;
+		}};
+		kacangHijau = new Item("kacang-hijau", Color.valueOf("c4e880")){{
+			cost = 0.1f;
+			hardness  = 1;
+		}};
+		// items end
+    }
+}
