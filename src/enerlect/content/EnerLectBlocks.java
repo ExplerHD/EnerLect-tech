2ba4ee1af0a10ca57ca90abadaf3f64e5436d4a4
1237 <92003875+12three7@users.noreply.github.com>
2022-03-06 14:04:40 +0800

    fix

--- src/enerlect/content/EnerLectBlocks.java
+++ src/enerlect/content/EnerLectBlocks.java
@@ -131,30 +131,23 @@ public class EnerLectBlocks implements ContentList{
     }};  //disabled due to nullpointerexception */
     lunarPanel = new LunarGenerator("lunar-panel"){{
       requirements(Category.power, with(Items.silicon, 15, EnerLectItems.darkenedMetal, 10, Items.lead, 15));
-      health = 40 * size * size;
+      health = 45 * size * size;
       size = 2;
       powerProduction = 0.27f;
     }};
     
     mediumLunarPanel = new LunarGenerator("medium-lunar-panel"){{
       requirements(Category.power, with(Items.silicon, 30, EnerLectItems.darkenedMetal, 15, Items.lead, 30, Items.metaglass, 15));
-      health = 40 * size * size;
+      health = 90 * size * size;
       size = 3;
       powerProduction = 0.7f;
     }};
 
     largeLunarPanel = new LunarGenerator("large-lunar-panel"){{
       requirements(Category.power, with(Items.silicon, 60, EnerLectItems.darkenedMetal, 30, Items.lead, 60, Items.metaglass, 30, Items.phaseFabric, 10));
-      health = 40 * size * size;
+      health = 90 * size * size;
       size = 4;
       powerProduction = 1f;
-    }};
-    
-    hugeLunarPanel = new LunarGenerator("huge-lunar-panel"){{
-      requirements(Category.power, with(Items.silicon, 100, EnerLectItems.darkenedMetal, 70, Items.lead, 100, Items.metaglass, 60, Items.phaseFabric, 30, Items.surgeAlloy, 10));
-      health = 40 * size * size;
-      size = 5;
-      powerProduction = 1.5f;
     }};
   }
 }
