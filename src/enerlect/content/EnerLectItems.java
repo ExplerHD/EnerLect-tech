12082e211f96ba7a67fec16484dad1613651e56a
1237 <92003875+12three7@users.noreply.github.com>
2022-03-12 19:38:18 +0800

    delete contents from lost tech #2

--- src/enerlect/content/EnerLectItems.java
+++ src/enerlect/content/EnerLectItems.java
@@ -14,21 +14,6 @@ public class EnerLectItems implements ContentList{
     darkenedMetal = new Item("darkened-metal", Color.valueOf("6b6464")){{
       cost = 4;
     }};
-    biostone = new Item("biostone", Color.valueOf("21de63")) {{
-      cost = 0;
-      flammability = 0.5f;
-      hardness = 0;
-    }};
-    fragments = new Item("fragments", Color.valueOf("b54ed4")) {{
-      cost = 0.5f;
-      charge = 0.15f;
-      hardness = 0;
-    }};
-    yellowsteel = new Item("yellow-steel", Color.valueOf("d9d35f")) {{
-      cost = 0.7f;
-      charge = 0.01f;
-      hardness = 2;
-    }};	
     // items end
     // liquids start
     electricLiquid = new Liquid("electric-liquid", Color.valueOf("f5372a")){{
