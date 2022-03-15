6a30cbc79657aa023f3000eda0eda139c973f3c3
ExplerHD <73381264+ExplerHD@users.noreply.github.com>
2022-03-15 18:54:17 +0700

    Update EnerLectFx.java

--- src/enerlect/graphics/EnerLectFx.java
+++ src/enerlect/graphics/EnerLectFx.java
@@ -31,16 +31,6 @@ public class EnerLectFx{
     color(Color.blue);
     stroke(3f * e.fout());
     Fill.poly(e.x, e.y, 6, e.rotation, e.fin());
-  }),
-  biostonegenerate = new Effect(90, e -> {
-    color(Color.valueOf("97d96f"));
-    alpha(e.fslope());
-
-    rand.setSeed(e.id);
-    for(int i = 0; i < 2; i++){
-      v.trns(rand.random(360f), rand.random(e.finpow() * 9f)).add(e.x, e.y);
-      Fill.circle(v.x, v.y, rand.random(1.4f, 2.4f));
-    }
-  }).layer(Layer.bullet - 1f);
+  })
 }
 
