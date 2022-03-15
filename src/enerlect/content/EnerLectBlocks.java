a608b872f2f8406e56aab996898d4a3b3ab7b6e5
ExplerHD <73381264+ExplerHD@users.noreply.github.com>
2022-03-15 21:56:14 +0700

    pt jayaputra

--- src/enerlect/content/EnerLectBlocks.java
+++ src/enerlect/content/EnerLectBlocks.java
@@ -79,7 +79,7 @@ public class EnerLectBlocks implements ContentList{
     }};
     darkenedMetalForge = new GenericCrafter("darkened-metal-forge"){{
       requirements(Category.crafting, with(Items.copper, 40, Items.lead, 30, Items.silicon, 40));
-      craftEffect = EnerLectFx.darkenedMetalForge;
+      craftEffect = EnerLectFx.dmfCrafted;
       outputItem = new ItemStack(EnerLectItems.darkenedMetal, 5);
       craftTime = 60f * 3;
       size = 3;
